7590d1ae722cda46cac7d3da90adc3cb5bfba9ca
yvana flayhan <flayhanyvana1@gmail.com>
2026-05-16 22:09:54 +0300

    Fixed delete data

--- Database/DeleteData.sql
+++ Database/DeleteData.sql
@@ -1,6 +1,12 @@
 -- Disable foreign key checks
 SET FOREIGN_KEY_CHECKS = 0;
 
+DELETE FROM VetDetails;
+DELETE FROM VetAnimalExpertise;
+DELETE FROM Attendance;
+DELETE FROM MedicalRecords;
+DELETE FROM PetStays;
+DELETE FROM PetStatus;
 DELETE FROM MedicalCases;
 DELETE FROM Appointments;
 DELETE FROM Pets;
