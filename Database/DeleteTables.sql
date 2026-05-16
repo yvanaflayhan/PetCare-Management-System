50051501294bd7ecfe6b0f9f2389ab6e1406c283
yvana flayhan <flayhanyvana1@gmail.com>
2026-05-16 22:01:49 +0300

    Fixed delete tables

--- Database/DeleteTables.sql
+++ Database/DeleteTables.sql
@@ -1,6 +1,12 @@
 -- Disable foreign key checks to avoid errors
 SET FOREIGN_KEY_CHECKS = 0;
 
+DROP TABLE IF EXISTS VetDetails;
+DROP TABLE IF EXISTS VetAnimalExpertise;
+DROP TABLE IF EXISTS Attendance;
+DROP TABLE IF EXISTS MedicalRecords;
+DROP TABLE IF EXISTS PetStays;
+DROP TABLE IF EXISTS PetStatus;
 DROP TABLE IF EXISTS MedicalCases;
 DROP TABLE IF EXISTS Appointments;
 DROP TABLE IF EXISTS Pets;
