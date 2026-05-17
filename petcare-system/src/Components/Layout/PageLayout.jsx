28411f61e01ee88807be24e2e2e175157267c33f
yvana flayhan <flayhanyvana1@gmail.com>
2026-05-18 01:01:58 +0300

    Added Layout components

---
+++ petcare-system/src/Components/Layout/PageLayout.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './Pagelayout.module.css';
+
+function PageLayout({ title, subtitle, action, children }) {
+  return (
+    <div className={styles.page}>
+      <div className={styles.content}>
+        
+        <div className={styles.header}>
+          
+          <div className={styles.headerText}>
+            <div className={styles.title}>{title}</div>
+            {subtitle && (
+              <div className={styles.sub}>{subtitle}</div>
+            )}
+          </div>
+
+          {action && (
+            <div className={styles.action}>
+              {action}
+            </div>
+          )}
+
+        </div>
+
+        <div className={styles.body}>
+          {children}
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default PageLayout;
